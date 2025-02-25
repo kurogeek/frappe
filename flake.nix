f702458f1bc2d25627fd1aa0bebb0a37e2ebb3d7
kurogeek <kurogeek@lmvhaus.com>
2025-02-25 13:59:43 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake.nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
+
+    devshell = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/devshell";
+    };
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { flake-parts, devshell, process-compose-flake, ... }@inputs:
+  flake-parts.lib.mkFlake { inherit inputs; }(
+    { lib, ... }: {
+      systems = [ "x86_64-linux" ];
+      imports = [ 
+        devshell.flakeModule
+        process-compose-flake.flakeModule
+      ];
+      perSystem = { pkgs, ... }: let
+        frappe-yarn = pkgs.mkYarnPackage {
+          pname = "frappe";
+          version = "1.1.1";
+          src = ./.;
+
+          packageJSON = ./package.json;
+          yarnLock = ./yarn.lock;
+        };
+
+      in {
+
+        
+
+        packages = {
+          frappe-yarn = frappe-yarn;
+
+          devshells.default = {
+            env = [
+
+            ];
+
+            packages = with pkgs; [
+              nodejs
+              turbo
+              python312
+            ];
+          };
+        };
+      };
+    }
+  );
+}
